4f811333faf5a6a7db1258b64cc01fe74ad67f90
putusel <elina.putus@gmail.com>
2022-09-29 17:44:46 +0300

    riippuvuudet lisätty

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { AsyncStorage } from'@react-native-async-storage/async-storage';
+import * as SQLite from'expo-sqlite';
 
 export default function App() {
   return (
